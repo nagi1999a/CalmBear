e749ce0744e4844b4fa52975cf1368cd68a8719f
Bo-Yi Tsai <49529145+nagi1999a@users.noreply.github.com>
2024-05-17 16:41:28 +0800

    Fix Typo

--- debian-install.sh
+++ debian-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install script for debian-based linux distributions
 dpkg --add-architecture i386 && apt-get update
-apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 --no-install-recommneds
+apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 --no-install-recommends
 cp -r output/i386/* /usr/
 cp -r output/x86_64/* /usr/
